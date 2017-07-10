0ec9056f48c7846de6a0e36e469dad5e0e16a750
Caleb Harada <caleb_harada@yahoo.com>
2017-07-10 12:19:39 -0500

    Add files via upload

---
+++ ViewImage.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+
+
+# Choose .FITS file
+filename = '...'
+hdu = fits.open(filename)
+data = hdu[0].data
+
+# display histogram
+plt.figure()
+plt.hist(np.asarray(data).flatten(),500)
+plt.yscale('Log')
+
+# display image
+plt.figure()
+plt.imshow(data, cmap='gray', vmin=np.median(data), vmax=np.median(data)+500)
+plt.colorbar()
+plt.title(file)
+
+plt.show()
